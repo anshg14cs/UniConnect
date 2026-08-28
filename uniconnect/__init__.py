9e4e2186309ad0b59663d7387a00e82c6ea0a7ec
Ansh <anshg140706@gmail.com>
2026-08-28 17:14:18 +0100

    Implemented backend for registration inc password hashing and duplicate emails

--- uniconnect/__init__.py
+++ uniconnect/__init__.py
@@ -1,6 +1,9 @@
 import os
-from flask import Flask, render_template, request
+import sqlite3
+from flask import Flask, render_template, request, redirect, url_for
 from .universities import UK_UNIVERSITIES
+from werkzeug.security import generate_password_hash
+from .db import get_db
 
 def create_app():
     app = Flask(__name__)
@@ -42,7 +45,25 @@ def create_app():
                 error = "Password must be at least 8 characters long."
 
             else:
-                print("Registration data is valid")
+                password_hash = generate_password_hash(password)
+                db = get_db()
+
+                try:
+
+                    db.execute(
+                        """
+                        INSERT INTO users (name, university, email, password_hash)
+                        VALUES (?, ?, ?, ?)
+                        """,
+                        (name, university, email, password_hash)
+                    )
+
+                    db.commit()
+                    return redirect(url_for("home"))
+
+                except sqlite3.IntegrityError:
+                    error = "An account with that email already exists." 
+                
         return render_template("register.html", error = error, name=name, email = email, university = university, universities = UK_UNIVERSITIES)
 
     return app
